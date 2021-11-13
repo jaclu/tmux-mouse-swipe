4f3046e0e2d730fb7fe262e36a9fe918e504f6d3
jaclu <jacob.lundqvist@gmail.com>
2021-11-13 03:07:00 +0100

    Added absolute dirs, so this will run even if called when not in this directory.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -5,7 +5,10 @@
 #
 #   Part of https://github.com/jaclu/tmux-mouse-swipe
 #
-#   Version: 1.0.0 2021-10-18
+#   Version: 1.1.0 2021-11-13
+#       Added absolute dirs, so this will run even if called when not
+#       in this directory.
+#     1.0.0 2021-10-18
 #       Initial deploy
 #
 #
@@ -19,25 +22,31 @@
 #
 #  Setup for running benchmarks:
 #
-#  You will want to examine the following settings in scripts/tmux_mouse_swiping
+#  You will want to examine the following settings in:
+#  scripts/tmux_mouse_swiping
 #
-#  benchmarking      Should be set to 1, otherwise this benchmark will try
-#                    to change the focus of the running tmux.
-#                    This benchmark is not meant to be testing how fast
-#                    tmux can switch contexts, it is just about how fast
-#                    mouse events are processed.
+#  benchmarking    Should be set to 1, otherwise this benchmark will try
+#                  to change the focus of the running tmux.
+#                  This benchmark is not meant to be testing how fast
+#                  tmux can switch contexts, it is just about how fast
+#                  mouse events are processed.
 #
-#                    Remember to set it back to 0 when you are done!
+#                  >>>  Remember to set it back to 0 when you are done!
 #
-#  drag_status_cache Experiment with enabling/disabling to see how much
-#                    performance gain it gives you, on my laptop it is some
-#                    20 times faster to use it. In the rare event it doesn't
-#                    improve things. you can always leave it disabled.
 #
-#  debug()           If you want to see what is happening consider enabling
-#                    this, on my machine this halves the perfomance of
-#                    the benchmark...
+#  drag_stat_cache_file
+#                  Experiment with enabling/disabling to see how much
+#                  performance gain it gives you, on my laptop it is some
+#                  20 times faster to use it. In the rare event it doesn't
+#                  improve things. you can always leave it disabled.
 #
+#  debug()         If you want to see what is happening consider enabling
+#                  this, on my machine this halves the perfomance of
+#                  the benchmark...
+#
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 
 
 #
@@ -46,37 +55,38 @@
 run_time=10
 
 
-../mouse_swipe.tmux  # reset env, especially force a env_check at first run
+# reset env, especially force a env_check at first run
+$(dirname "$CURRENT_DIR")/mouse_swipe.tmux
 
 
 #
 # First do one run, so env should have been picked up before the loop
 #
-./tmux_mouse_swiping down "2" "2"
-echo
-./tmux_mouse_swiping up "4" "2"
+$CURRENT_DIR/tmux_mouse_swiping down 2 2
+$CURRENT_DIR/tmux_mouse_swiping up 4 2
 
 
-echo "--- starting mouse swiping ---"
+echo "--- Will do mouse swiping for ${run_time}s ---"
 t_start="$(date +%s)"
+t_end="$(( t_start + run_time ))"
 
 x=0
 while :; do
     x=$((x+1))
-    ./tmux_mouse_swiping down "$x" "5"
+    $CURRENT_DIR/tmux_mouse_swiping down $x 5
     #
     # Calculating duration and comparing it inside the loop gives something
     # like a 25% overhead, but since it is constant on a given system,
-    # it doesnt really matter. The interesting part is not the absoulute number,
-    # it is how it changes depending on code tweaks.
+    # it doesnt really matter. The interesting part is not
+    # the absoulute number, it is how it changes depending on code tweaks.
     #
-    duration=$(( $(date +%s) - t_start ))
-    [ "$duration" -ge "$run_time" ] && break
+    [ "$(date +%s)" -ge "$t_end" ] && break
 done
-
 echo "---  ending swipe  ---"
-./tmux_mouse_swiping up "5" "5"
+
+$CURRENT_DIR/tmux_mouse_swiping up "5" "5"
 
 echo "Made $x swipe steps in ${duration} seconds"
 
-../mouse_swipe.tmux  # reset env again
+
+$(dirname "$CURRENT_DIR")/mouse_swipe.tmux  # reset env again
